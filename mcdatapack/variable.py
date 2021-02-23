ab51a47d21e6f58f83c55752e5c8893351d0bc7d
Ovizro <1746672572@qq.com>
2021-02-23 21:39:32 +0800

    update variable

--- mcdatapack/variable.py
+++ mcdatapack/variable.py
@@ -1,8 +1,45 @@
+import ujson
+from typing import Any, Dict
+
+class Counter:
+	
+	__slots__ = ["_count",]
+	
+	def __init__(self):
+		self._count = 0
+	
+	def __pos__(self) -> None:
+		self._count += 1
+		
+	def __neg__(self) -> None:
+		self._count -= 1
+		
+	def __invert__(self) -> None:
+		self._count = 0
+		
+	def __bool__(self) -> bool:
+		if self._count > 0:
+			return True
+		else:
+			return False
 
 class Variable:
-  
-  def __init__(self) -> None:
-    self.count = 0
+    
+    __slots__ = ["counter",]
+	
+	def __init__(self) -> None:
+		self.counter = Counter()
 
-class ScoreboardType:
-  
+class ScoreboardType(Variable):
+    
+    def __init__(
+        self,
+        name: str,
+        criteria: str = "dummy",
+        display: dict
+    ):
+        self.name = name
+        self.criteria = criteria
+        self.display = display
+        
+        self.counter = Counter()
