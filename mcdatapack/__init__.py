16d71e49ebdf633f62ea15983364dc4b61609238
Ovizro <1746672572@qq.com>
2021-05-03 19:45:57 +0800

    update

--- mcdatapack/__init__.py
+++ mcdatapack/__init__.py
@@ -1,13 +1,12 @@
-from .file_output import FileOutput
 from .file_struct import BuildDirs
-from .file import comment, insert
+from .context import get_context, comment, insert
 from .exception import McdpError
 
 __version__ = "Alpha 0.1.0"
 __version_num__ = 100
 
 __all__ = [
-    "FileOutput",
+    "get_context",
     "BuildDirs",
     "comment",
     "insert",
--- mcdatapack/exception.py
+++ mcdatapack/exception.py
@@ -1,14 +1,25 @@
 import sys
 from typing import Union
 
-#from . import __version__ as VERSION
-VERSION = "Alpha 0.1.0"
+from . import __version__ as VERSION
+from . import get_context
+
 class McdpError(Exception):
+    
+    __slots__ = ["mcdp_version", "python_version"]
     
     def __init__(self, arg: str) -> None:
         self.mcdp_version = VERSION
         self.python_version = sys.version
         super().__init__(arg)
+
+class McdpContextError(McdpError, OSError):
+	
+	__slots__ = ["context", ]
+	
+	def __init__(self, arg: str) -> None:
+		self.context = get_context()
+		super().__init__(arg)
 
 class MinecraftVersionError(McdpError):
     pass
