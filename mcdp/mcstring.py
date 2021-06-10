d76c6916cd9eff19b9fcfc538a264f26e5a6249f
Ovizro <1746672572@qq.com>
2021-06-11 07:59:20 +0800

    update

--- mcdp/mcstring.py
+++ mcdp/mcstring.py
@@ -1,4 +1,4 @@
-from typing import Dict, Literal, Union, Optional
+from typing import Dict, List, Literal, Union, Optional
 
 from .typings import McdpBaseModel
 from .config import check_mc_version
@@ -13,7 +13,7 @@ class Score(MCStringObj):
     objective: str
     value: str
     
-class clickEvent(MCStringObj):
+class ClickEvent(MCStringObj):
     action: Literal["open_url", "run_command", "suggest_command", "change_page", "copy_to_clipboard"]
     value: str
 
@@ -26,7 +26,7 @@ def _hover_event_checker(
     if action == 'show_text':
         pass
 
-class hoverEvent(MCStringObj):
+class HoverEvent(MCStringObj):
     action: Literal["show_text", "show_item", "show_entity"]
     value: Optional[str] = None
     contents: Optional[Union[str, list, dict]] = None
@@ -38,4 +38,59 @@ class hoverEvent(MCStringObj):
         value: Optional[str] = None,
         contents: Optional[Union[str, list, dict]] = None
     ) -> None:
-        super().__init__(**data)
+        super().__init__(action=action, value=value,contents=contents)
+
+_stand_color = ("black", "dark_blue", "dark_green", "dark_aqua", "dark_red", "dark_purple",
+            "gold", "gray", "dark_gray", "blue", "green", "aqua", "red", "light_purple",
+            "yellow", "white", "reset")
+
+@check_mc_version('>=1.16')
+def _check_color(color: str) -> None:
+    if not color in _stand_color and color.startswith('#'):
+        try:
+            int(color[1:])
+        except (TypeError, ValueError):
+            pass
+        else:
+            return
+    elif color in _stand_color:
+        return
+    
+    raise ValueError("invalid string attrs 'color'.")
+    
+@check_mc_version('<1.16')
+def _check_color(color: str) -> None:
+    if not color in _stand_color:
+        raise ValueError("invalid string attrs 'color'.")
+
+class MCSS(MCStringObj):
+    color: str = None
+    bold: bool = None
+    italic: bool = None
+    underlined: bool = None
+    strikethrough: bool = None
+    obfuscated: bool = None
+    font: str = None
+    separator: Union[str, dict] = None
+    insertion: str = None
+    clickEvent: ClickEvent = None
+    hoverEvent: HoverEvent = None
+    
+    def __call__(self, text: Optional[str] = None, **kw):
+        if text:
+            kw["text"] = text
+        return MCString(**self.dict(), **kw)
+
+class MCString(MCSS):
+    text: str = None
+    translate: str = None
+    with_: List[str] = None
+    score: Score = None
+    selector: str = None
+    keybind: str = None
+    nbt: str = None
+    block: str = None
+    entity: str = None
+    storage: str = None
+    extra: List["MCString"] = None
+    
