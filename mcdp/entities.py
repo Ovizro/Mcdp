736e388b2b66384f3b2d892855aa7bcf4d0d2180
Ovizro <1746672572@qq.com>
2021-06-19 21:31:01 +0800

    update

--- mcdp/entities.py
+++ mcdp/entities.py
@@ -1,67 +1,10 @@
 import ujson
 from io import StringIO
 from typing import Literal, Optional, Union, Dict
+
 from .context import insert
+from .command import Position
 from .typings import Variable, McdpVar
-
-class PosComponent(McdpVar):
-    
-    __slots__ = ["type", "value"]
-    
-    def __init__(
-        self,
-        value: str,
-        type: Optional[Literal["absolute", "relative", "local"]] = None
-    ) -> None:
-        self.value = value
-        if '^' in value:
-            int(value[1:])
-            self.type = "local"
-        elif '~' in value:
-            int(value[1:])
-            self.type = "relative"
-        else:
-            int(value)
-            self.type = "absolute"
-        if type and (type != self.type):
-            raise ValueError("unsuit position value.")
-        
-    def __str__(self) -> str:
-        return self.value
-    
-    __repr__ = __str__
-
-class Position(McdpVar):
-
-    __slots__ = ["_posXYZ", "type"]
-
-    def __init__(self, pos: str) -> None:
-        l = [PosComponent(i) for i in pos.split()]
-        if len(l) != 3:
-            raise ValueError("incorrect position length.")
-        
-        tid = 0
-        for i in l:
-            if i.type == "absolute":
-                if tid == 3:
-                    raise TypeError
-                tid = 1
-            elif i.type == "relative":
-                if tid == 3:
-                    raise TypeError
-                tid = 2
-            else:
-                if tid < 3 and tid != 0:
-                    raise TypeError
-                tid = 3
-        
-        self._posXYZ = tuple(l)
-        
-    def __repr__(self) -> str:
-        return f"Position{self._posXYZ}"
-    
-    def __str__(self) -> str:
-        return " ".join([i.value for i in self._posXYZ])
         
 class Entity(Variable):
     
