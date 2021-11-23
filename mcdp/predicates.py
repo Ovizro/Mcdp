a1c6f45f7b8d2e9cb8ab2fde91778bb12732a97f
Ovizro <1746672572@qq.com>
2021-11-23 21:30:38 +0800

    emm some bugs...

--- mcdp/predicates.py
+++ mcdp/predicates.py
@@ -4,4 +4,4 @@ from .aio_stream import Stream
 from .context import get_namespace
 
 def get_predicate_stream(name: str) -> Stream:
-    return Stream(name, root=get_namespace()+"/predicates")
+    return Stream(name+".json", root=get_namespace()+"/predicates")
