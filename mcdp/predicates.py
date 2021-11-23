e08b2abda5f2ea0e7bc729cd0dcdf5685d120cd7
Ovizro <1746672572@qq.com>
2021-11-23 21:28:56 +0800

    add predicates.py

---
+++ mcdp/predicates.py
@@ -0,0 +1,7 @@
+from typing import Any, Dict
+
+from .aio_stream import Stream
+from .context import get_namespace
+
+def get_predicate_stream(name: str) -> Stream:
+    return Stream(name, root=get_namespace()+"/predicates")
